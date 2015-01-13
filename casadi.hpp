ccf0408d45a364ca893e4cbd014b08b9469ba462
Abuenameh Aiyejina <abuenameh@gmail.com>
2015-01-13 17:20:11 -0400

    Commit

--- casadi.hpp
+++ casadi.hpp
@@ -16,30 +16,14 @@ using namespace casadi;
 
 using namespace boost::posix_time;
 
-//#include <pagmo/src/pagmo.h>
-//
-//using namespace pagmo;
-//using namespace pagmo::algorithm;
-//using namespace pagmo::problem;
-
 #include "gutzwiller.hpp"
 
 class GroundStateProblem {
 public:
     GroundStateProblem();
     
-//    problem::base_ptr clone() const { return problem::base_ptr(new GroundStateProblem(*this));s }
-//    
-//    void objfun_impl(fitness_vector& f, const decision_vector& x) const {
-//        vector<double> grad;
-//        double En = const_cast<GroundStateProblem*>(this)->E(x, grad);
-//        f[0] = En;
-//    }
-    
     void setParameters(double U0, vector<double>& dU, vector<double>& J, double mu);
     void setTheta(double theta);
-    
-    double solve(vector<double>& f);
     
     double E(const vector<double>& f, vector<double>& grad);
     
@@ -49,12 +33,15 @@ public:
     void start() { start_time = microsec_clock::local_time(); }
     void stop() { stop_time = microsec_clock::local_time(); }
     
+    static void setup();
+    
 private:
     
     ptime start_time;
     ptime stop_time;
     
-    SX energy();
+    static SX energy(vector<SX>& fin, vector<SX>& J, SX& U0, vector<SX>& dU, SX& mu, SX& theta);
+    static SX energy(int i, int n, vector<SX>& fin, vector<SX>& J, SX& U0, vector<SX>& dU, SX& mu, SX& theta);
     
     vector<SX> fin;
     SX U0;
@@ -68,30 +55,12 @@ private:
     
     vector<double> params;
     
-    SXFunction Ef;
-    Function Egradf;
-    NlpSolver nlp;
+    vector<Function> Ef;
+    vector<Function> Egradf;
     
     string status;
     double runtime;
 };
-
-//class energyprob : public problem::base {
-//public:
-//    energyprob(int n/*, GroundStateProblem* prob_*/) : base(n)/*, prob(prob_)*/ {}
-//    problem::base_ptr clone() const { return problem::base_ptr(new energyprob(*this)); }
-//    
-//    void setProblem(GroundStateProblem* prob_) { prob = prob_; }
-//    
-//    void objfun_impl(fitness_vector& f, const decision_vector& x) const {
-//        vector<double> grad;
-//        double En = prob->E(x, grad);
-//        f[0] = En;
-//    }
-//    
-//private:
-//    GroundStateProblem* prob;
-//};
 
 double energyfunc(const vector<double>& x, vector<double>& grad, void *data);
 
